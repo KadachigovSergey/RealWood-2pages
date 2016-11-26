0cb87d872dc6c9fe501ebef6b1dbd0a0b465a02d
MrKadachigov <kadachigovs@gmail.com>
2016-11-26 17:35:16 +0200

    Start RealWood

---
+++ dist/js/plugin.js
@@ -0,0 +1,12 @@
+;(function ($){
+    $(window).on('load', function(){
+        $('select').styler();
+    })
+
+    var sidebarBtn = $('.sidebar-btn');
+    var sidebar = $('.sidebar');
+    sidebarBtn.on('click', function (){
+        sidebarBtn.toggleClass('open');
+        sidebar.toggleClass('open');
+    });
+})(jQuery);
