aca84a641e21b6b4a4e376dc0cbffd2499ffd4f3
MrKadachigov <kadachigovs@gmail.com>
2016-11-28 16:34:06 +0200

    End products page

--- dist/js/plugin.js
+++ dist/js/plugin.js
@@ -1,12 +1,12 @@
 ;(function ($){
     $(window).on('load', function(){
-        $('select').styler();
-    })
-
-    var sidebarBtn = $('.sidebar-btn');
-    var sidebar = $('.sidebar');
-    sidebarBtn.on('click', function (){
-        sidebarBtn.toggleClass('open');
-        sidebar.toggleClass('open');
+        // $('select').styler();
+        var sidebarBtn = $('.sidebar-btn');
+        var sidebar = $('.sidebar');
+        sidebarBtn.on('click', function (){
+            sidebarBtn.toggleClass('open');
+            sidebar.toggleClass('open');
+        });
     });
+
 })(jQuery);
